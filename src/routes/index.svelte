77e0c9fd52af7293bd51a383b4cac5014c8f2e54
Rainer Simon <aboutgeo@gmail.com>
2022-02-01 09:49:32 +0100

    Fixed typo

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -92,7 +92,7 @@
         Linking Viennese Art through AI
       </h1>
       <p class="leading-normal text-2xl mb-8">
-        A research collaboration that investigates how Artificial IntelligencW 
+        A research collaboration that investigates how Artificial Intelligence 
         can help interlink, navigate and analyse art collections.
       </p>
       <!-- button class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
