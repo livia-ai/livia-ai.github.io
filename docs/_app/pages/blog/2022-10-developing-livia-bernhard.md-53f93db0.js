b71195c1489a5d02efbf00f338a269b84419a88c
Rainer Simon <aboutgeo@gmail.com>
2022-10-06 09:57:36 +0200

    Site deploy

---
+++ docs/_app/pages/blog/2022-10-developing-livia-bernhard.md-53f93db0.js
@@ -0,0 +1,93 @@
+import{S as Be,i as Ee,s as Le,C as le,w as Pe,x as je,y as Te,z as He,A as _e,q as Fe,o as Me,B as $e,Q as Ae,e as h,t as s,k as u,c,a as m,h as l,d as t,m as f,b as g,g as o,H as i}from"../../chunks/vendor-f435face.js";import{P as ze}from"../../chunks/_post-4f5578a2.js";function Se(k){let n,y,p,I,r,d,_,Q,$,A,R,z,x,V,S,w,G,b,X,Y,q,B,Z,C,E,ee,D,L,te,W,P,ae,O,j,ne,J,T,ie,K,H,v,M,oe,re,F,se;return{c(){n=h("p"),y=s(`We are about half-way through our project and, unfortunately, this means that our intern Bernhard has
+completed his internship, and is leaving LiviaAI. Bernhard\u2019s skills, creativity and commitment have
+been crucial to our effort. His uncomplicated, down-to-earth style, and his ability to explain complex
+things with ease and clarity have made working with him a pleasure. (Trust me, I have learned way more
+from him than vice versa!) All of the cool stuff showcased on our blog -
+`),p=h("a"),I=s("collection metadata sentence embeddings"),r=s(` or
+the `),d=h("a"),_=s("automated triplet selection"),Q=s(` - are his
+work! Naturally, we are sad to let him go. But the good news is: there\u2019s a ton of stuff that Bernhard has
+created, which we haven\u2019t shared with you so far. The best is yet to come! But first, before letting
+Bernhard off the hook & back to university, we have asked him to write about his internship
+experience, and tell us about what LiviaAI was like for him!`),$=u(),A=h("h2"),R=s("A Personal Conclusion on the Implementation of LiviaAI"),z=u(),x=h("h4"),V=s("By Bernhard Franzl"),S=u(),w=h("p"),G=s(`My name is Bernhard Franzl and I am currently finishing my Bachelor\u2019s degree in
+`),b=h("a"),X=s("Artificial Intelligence at the Johannes Kepler University in Linz"),Y=s(`.
+I am currently writing my thesis, and I am about to start my Master\u2019s in
+the upcoming winter semester. In the six semesters I have studied so far, I
+got to know a lot of different aspects of artificial intelligence. However,
+many topics are only covered briefly, and with little reference to practical
+application. Exercise courses, which focus on practice and implementation,
+are only held for selected important aspects of Machine Learning. I assume
+the reason is that the university simply does not have the resources to go
+into more detail. Moreover, I think the tasks - and especially datasets - in
+the exercises are a bit too simplified and with little practical relevance.
+Therefore, I felt the need to work on something more realistic, and I began
+searching for an internship.`),q=u(),B=h("h2"),Z=s("LiviaAI"),C=u(),E=h("p"),ee=s(`I started working on the LiviaAI project in April 2022, and was instantly
+excited about it. My initial challenge was to find out how I could apply
+what I had learned at university to the tasks of LiviaAI. However, I quickly
+realised that my practical university knowledge would fit perfectly, and
+that LiviaAI was in fact a great opportunity to test my knowledge - and
+get some feedback on the way!`),D=u(),L=h("p"),te=s(`I found it refreshing to work with real-world cultural data. It was fascinating
+to get a look inside the museum collections. I think that the connection
+between culture and data science is what made this internship so interesting
+to me. In particular, I also enjoyed the opportunity to work with visual
+arts data, because I am particularly interested in computer vision and
+applications that involve image data.`),W=u(),P=h("p"),ae=s(`I think that, most of all, my internship has improved my programming skills.
+Working constantly on the project\u2019s many small programming tasks made it a
+routine. Furthermore, it was interesting to work with PyPI (the Python
+package management repository) to publish the main LiviaAI code as a Python
+package. This was a great opportunity to learn the basics about Python
+package maintenance.`),O=u(),j=h("p"),ne=s(`Even though I feel I was well prepared for the project, I still had to do a
+lot of research. This was admittedly sometimes boring. But I learned about a
+lot of new algorithms, models and concepts. For me the most fascinating ones
+were Sentence Transformers, because they are just extremely useful. At the
+university, we were only briefly introduced to the underlying mechanism.
+Doing more research and applying them in practice turned out to be very
+interesting, and I gained a lot of new insights.`),J=u(),T=h("p"),ie=s(`All in all, I am very satisfied with my internship at the Austrian Institute
+of Technology. It fulfilled all my expectations, and gave me the opportunity
+to test my knowledge on a challenging real-world problem. I am happy that
+I had the chance to be a part of LiviaAI.`),K=u(),H=h("footer"),v=h("div"),M=h("h4"),oe=s("Previous post"),re=u(),F=h("a"),se=s("Exploring Similarity"),this.h()},l(e){n=c(e,"P",{});var a=m(n);y=l(a,`We are about half-way through our project and, unfortunately, this means that our intern Bernhard has
+completed his internship, and is leaving LiviaAI. Bernhard\u2019s skills, creativity and commitment have
+been crucial to our effort. His uncomplicated, down-to-earth style, and his ability to explain complex
+things with ease and clarity have made working with him a pleasure. (Trust me, I have learned way more
+from him than vice versa!) All of the cool stuff showcased on our blog -
+`),p=c(a,"A",{href:!0,rel:!0});var he=m(p);I=l(he,"collection metadata sentence embeddings"),he.forEach(t),r=l(a,` or
+the `),d=c(a,"A",{href:!0,rel:!0});var ce=m(d);_=l(ce,"automated triplet selection"),ce.forEach(t),Q=l(a,` - are his
+work! Naturally, we are sad to let him go. But the good news is: there\u2019s a ton of stuff that Bernhard has
+created, which we haven\u2019t shared with you so far. The best is yet to come! But first, before letting
+Bernhard off the hook & back to university, we have asked him to write about his internship
+experience, and tell us about what LiviaAI was like for him!`),a.forEach(t),$=f(e),A=c(e,"H2",{});var me=m(A);R=l(me,"A Personal Conclusion on the Implementation of LiviaAI"),me.forEach(t),z=f(e),x=c(e,"H4",{});var de=m(x);V=l(de,"By Bernhard Franzl"),de.forEach(t),S=f(e),w=c(e,"P",{});var N=m(w);G=l(N,`My name is Bernhard Franzl and I am currently finishing my Bachelor\u2019s degree in
+`),b=c(N,"A",{href:!0,rel:!0});var pe=m(b);X=l(pe,"Artificial Intelligence at the Johannes Kepler University in Linz"),pe.forEach(t),Y=l(N,`.
+I am currently writing my thesis, and I am about to start my Master\u2019s in
+the upcoming winter semester. In the six semesters I have studied so far, I
+got to know a lot of different aspects of artificial intelligence. However,
+many topics are only covered briefly, and with little reference to practical
+application. Exercise courses, which focus on practice and implementation,
+are only held for selected important aspects of Machine Learning. I assume
+the reason is that the university simply does not have the resources to go
+into more detail. Moreover, I think the tasks - and especially datasets - in
+the exercises are a bit too simplified and with little practical relevance.
+Therefore, I felt the need to work on something more realistic, and I began
+searching for an internship.`),N.forEach(t),q=f(e),B=c(e,"H2",{});var ue=m(B);Z=l(ue,"LiviaAI"),ue.forEach(t),C=f(e),E=c(e,"P",{});var fe=m(E);ee=l(fe,`I started working on the LiviaAI project in April 2022, and was instantly
+excited about it. My initial challenge was to find out how I could apply
+what I had learned at university to the tasks of LiviaAI. However, I quickly
+realised that my practical university knowledge would fit perfectly, and
+that LiviaAI was in fact a great opportunity to test my knowledge - and
+get some feedback on the way!`),fe.forEach(t),D=f(e),L=c(e,"P",{});var ye=m(L);te=l(ye,`I found it refreshing to work with real-world cultural data. It was fascinating
+to get a look inside the museum collections. I think that the connection
+between culture and data science is what made this internship so interesting
+to me. In particular, I also enjoyed the opportunity to work with visual
+arts data, because I am particularly interested in computer vision and
+applications that involve image data.`),ye.forEach(t),W=f(e),P=c(e,"P",{});var we=m(P);ae=l(we,`I think that, most of all, my internship has improved my programming skills.
+Working constantly on the project\u2019s many small programming tasks made it a
+routine. Furthermore, it was interesting to work with PyPI (the Python
+package management repository) to publish the main LiviaAI code as a Python
+package. This was a great opportunity to learn the basics about Python
+package maintenance.`),we.forEach(t),O=f(e),j=c(e,"P",{});var ve=m(j);ne=l(ve,`Even though I feel I was well prepared for the project, I still had to do a
+lot of research. This was admittedly sometimes boring. But I learned about a
+lot of new algorithms, models and concepts. For me the most fascinating ones
+were Sentence Transformers, because they are just extremely useful. At the
+university, we were only briefly introduced to the underlying mechanism.
+Doing more research and applying them in practice turned out to be very
+interesting, and I gained a lot of new insights.`),ve.forEach(t),J=f(e),T=c(e,"P",{});var ge=m(T);ie=l(ge,`All in all, I am very satisfied with my internship at the Austrian Institute
+of Technology. It fulfilled all my expectations, and gave me the opportunity
+to test my knowledge on a challenging real-world problem. I am happy that
+I had the chance to be a part of LiviaAI.`),ge.forEach(t),K=f(e),H=c(e,"FOOTER",{});var Ie=m(H);v=c(Ie,"DIV",{class:!0});var U=m(v);M=c(U,"H4",{});var be=m(M);oe=l(be,"Previous post"),be.forEach(t),re=f(U),F=c(U,"A",{href:!0});var ke=m(F);se=l(ke,"Exploring Similarity"),ke.forEach(t),U.forEach(t),Ie.forEach(t),this.h()},h(){g(p,"href","https://livia-ai.github.io/blog/2022-04-machines-reading-metadata/"),g(p,"rel","nofollow"),g(d,"href","https://livia-ai.github.io/blog/2022-06-exploring-similarity/"),g(d,"rel","nofollow"),g(b,"href","https://www.jku.at/en/degree-programs/types-of-degree-programs/bachelors-and-diploma-degree-programs/ba-artificial-intelligence/"),g(b,"rel","nofollow"),g(F,"href","/blog/2022-06-exploring-similarity/"),g(v,"class","previous-post")},m(e,a){o(e,n,a),i(n,y),i(n,p),i(p,I),i(n,r),i(n,d),i(d,_),i(n,Q),o(e,$,a),o(e,A,a),i(A,R),o(e,z,a),o(e,x,a),i(x,V),o(e,S,a),o(e,w,a),i(w,G),i(w,b),i(b,X),i(w,Y),o(e,q,a),o(e,B,a),i(B,Z),o(e,C,a),o(e,E,a),i(E,ee),o(e,D,a),o(e,L,a),i(L,te),o(e,W,a),o(e,P,a),i(P,ae),o(e,O,a),o(e,j,a),i(j,ne),o(e,J,a),o(e,T,a),i(T,ie),o(e,K,a),o(e,H,a),i(H,v),i(v,M),i(M,oe),i(v,re),i(v,F),i(F,se)},d(e){e&&t(n),e&&t($),e&&t(A),e&&t(z),e&&t(x),e&&t(S),e&&t(w),e&&t(q),e&&t(B),e&&t(C),e&&t(E),e&&t(D),e&&t(L),e&&t(W),e&&t(P),e&&t(O),e&&t(j),e&&t(J),e&&t(T),e&&t(K),e&&t(H)}}}function qe(k){let n,y;const p=[k[0],xe];let I={$$slots:{default:[Se]},$$scope:{ctx:k}};for(let r=0;r<p.length;r+=1)I=le(I,p[r]);return n=new ze({props:I}),{c(){Pe(n.$$.fragment)},l(r){je(n.$$.fragment,r)},m(r,d){Te(n,r,d),y=!0},p(r,[d]){const _=d&1?He(p,[d&1&&_e(r[0]),d&0&&_e(xe)]):{};d&2&&(_.$$scope={dirty:d,ctx:r}),n.$set(_)},i(r){y||(Fe(n.$$.fragment,r),y=!0)},o(r){Me(n.$$.fragment,r),y=!1},d(r){$e(n,r)}}}const xe={title:"Developing LiviaAI",description:"A personal conclusion on the implementation of LiviaAI written by our intern Bernhard Franzl.",url:"https://liviaai.at/blog/2022-09-developing-livia-bernhard",date:"October 06, 2022"};function Ce(k,n,y){return k.$$set=p=>{y(0,n=le(le({},n),Ae(p)))},n=Ae(n),[n]}class Oe extends Be{constructor(n){super();Ee(this,n,Ce,qe,Le,{})}}export{Oe as default,xe as metadata};
